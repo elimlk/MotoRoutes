f3733a5bd31a8da8316885eb88952d9ac445fca3
Itamar Yacoby <ithamar000@yahoo.com>
2021-05-22 16:24:26 +0300

    fragment login to logged in navigation completed (again)

--- app/src/main/java/com/motoroutes/view/LoggedInFragment.java
+++ app/src/main/java/com/motoroutes/view/LoggedInFragment.java
@@ -2,18 +2,26 @@ package com.motoroutes.view;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseUser;
 import com.motoroutes.R;
 import com.motoroutes.viewmodel.LoggedInViewModel;
@@ -25,11 +33,16 @@ public class LoggedInFragment extends Fragment {
     private TextView loggedInUserTextView;
     private Button logOutButton;
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
     private LoggedInViewModel loggedInViewModel;
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         loggedInViewModel =  new ViewModelProvider(this).get(LoggedInViewModel.class);
         loggedInViewModel.getUserMutableLiveData().observe(this, new Observer<FirebaseUser>() {
@@ -61,6 +74,16 @@ public class LoggedInFragment extends Fragment {
 
 
         View view = inflater.inflate(R.layout.fragment_logged_in, container,false);
+
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+        navigationView = view.findViewById(R.id.nav_view);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24);
+
         loggedInUserTextView = view.findViewById(R.id.fragment_loggedin_loggedInUser);
         logOutButton = view.findViewById(R.id.fragment_loggedin_logOut);
 
@@ -70,6 +93,34 @@ public class LoggedInFragment extends Fragment {
                 loggedInViewModel.logOut();
             }
         });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                int itemId = item.getItemId();
+                switch (itemId){
+                    case R.id.item_routes:
+                        Toast.makeText(getContext(),"routes",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item_addRoute:
+                        Toast.makeText(getContext(),"addRoutes",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item_emergency:
+                        Toast.makeText(getContext(),"emergency",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
+
+
         return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull @NotNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+
     }
 }
